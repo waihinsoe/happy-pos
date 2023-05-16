93086cbb0859814fa1761cc78b7b31c5f45e4de1
waihinsoe <waihinsoe332004@gmail.com>
2023-05-16 20:21:58 +0630

    feat: added env key for database

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,10 +3,18 @@ interface Config {
   spaceAccessKeyId: string;
   spaceSecretAccessKey: string;
   spaceEndPoint: string;
+  dataBaseHostUrl: string;
+  dataBaseUserName: string;
+  dataBasePassword: string;
+  dataBaseName: string;
 }
 export const config: Config = {
   jwtSecret: process.env.JWT_SECRET || "",
   spaceAccessKeyId: process.env.SPACE_ACCESS_KEY_ID || "",
   spaceSecretAccessKey: process.env.SPACE_SECRET_ACCESS_KEY || "",
   spaceEndPoint: process.env.SPACE_ENDPOINT || "",
+  dataBaseHostUrl: process.env.DATABASE_HOST_URL || "",
+  dataBaseUserName: process.env.DATABASE_USERNAME || "",
+  dataBasePassword: process.env.DATABASE_PASSWORD || "",
+  dataBaseName: process.env.DATABASE_NAME || "",
 };
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,10 +1,11 @@
 import { Pool } from "pg";
+import { config } from "../config/config";
 
 export const pool = new Pool({
-  host: "0.0.0.0",
-  user: "postgres",
-  password: "waihinsoe",
-  database: "my_test_db",
+  host: config.dataBaseHostUrl,
+  user: config.dataBaseUserName,
+  password: config.dataBasePassword,
+  database: config.dataBaseName,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
